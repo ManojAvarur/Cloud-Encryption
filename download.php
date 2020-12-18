296fbbf510fcbb0d6f1a129b5278cfc8e32b537f
unknown <Admin@LAP32.CONCORDEGROUP.CO.IN>
2020-12-18 23:30:42 +0530

    Download functionality added

---
+++ download.php
@@ -0,0 +1,34 @@
+<?php
+
+    if( isset( $_GET['file'] ) && !empty( $_GET['file'] ) ){
+
+        $file = 'Encrypted/'.$_GET['file'];
+
+        if( file_exists( $file ) ){
+
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename( $file ).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize( $file ));
+            readfile( $file );
+            ignore_user_abort(true);
+            unlink( $file );
+
+            if( connection_aborted() )
+                unlink( $file );
+            
+
+        }  else {
+
+            echo '<script>
+                    alert("File has been downloaded");
+                    window.history.go(-2);    
+                </script>
+            ';
+
+        }
+
+    }
--- upload.php
+++ upload.php
@@ -100,9 +100,7 @@
             <div class="container">
 
                     <div class="card text-center" style="background-color:  #1d1d1d;">
-                        <div class="card-header">
-                            <h1 style="text-decoration: underline;">Download</h1>
-                        </div>
+
 
                     <?php 
 
@@ -110,29 +108,34 @@
 
                         if( $type == 'Encrypt' ){
 
-                            // echo "
-                            //     <div class='card-body'>
-                            //         <h5 class='card-title'> " . $file['name'] . " </h5>
-                            //         <h5 class='card-title'>Password :  <span style='color: red'>" . $password . "</span> </h5>
-                            //         <p class='card-text'>Your File Is Ready To Download</p>
-                            //         <a href='#' class='btn btn-primary'>Download</a>
-                            //     </div>
-                            //     <div class='card-footer text-muted'>
-                            //     " . $filesize . " KB
-                            //     </div>
-                            // ";
-
                             echo "
+                                 <div class='card-header'>
+                                     <h1 style='text-decoration: underline;'>Download</h1>
+                                </div>
                                 <div class='card-body'>
-                                    <h5 class='card-title'>Special title treatment</h5>
-                                    <p class='card-text'>With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href='#' class='btn btn-primary'>Go somewhere</a>
+                                    <h5 class='card-title'> " . $file['name'] . " </h5>
+                                    <h5 class='card-title'>Password :  <span style='color: red' id='password'>" . $password . "</span></h5>
+                                    <!-- <input type='hidden' id='password' value='" . $password . "'> -->
+                                    <button class='btn btn-primary' style='margin-bottom: 10px;' onclick='copyPassword()' id='copyPassword'>Copy Password</button>
+                                    <p class='card-text'>Your File Is Ready To Download</p>
+                                    <a href='download.php?file=" . $file['name'] . "' onclick='downloaded()' id='downCompleted' class='btn btn-primary'>Download</a>
+                                </div>
+                                <div class='card-footer text-muted'>
+                                " . $filesize . " KB
                                 </div>
                             ";
 
                         }
 
                     } else {
+
+                        echo "
+                        <div class='card-body'>
+                            <h5 class='card-title'>Who are you?</h5>
+                            <p class='card-text'>We could'nt find any of your files on our server. Please try again!</p>
+                            <a href='index.html' class='btn btn-primary'>Go Back</a>
+                        </div>
+                    ";
 
                     }
                         
@@ -156,10 +159,105 @@
             </div>
         </div>
 </body>
+<script>
+    <?php
+        if( ! $fileNotExist ){
+
+            if( $type == 'Encrypt' ){
+    ?>
+                function copyPassword() {
+                    var tempInput = document.createElement("input");
+                    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+                    tempInput.value = '<?php echo $password ?>';
+                    document.body.appendChild(tempInput);
+                    tempInput.select();
+                    document.execCommand("copy");
+                    document.body.removeChild(tempInput);
+                    button = document.getElementById('copyPassword');
+                    button.innerHTML = 'Copied ✔';
+                    button.classList.add('btn-success');
+                }
+
+                function downloaded(){
+                    button = document.getElementById('downCompleted');
+                    button.innerHTML = 'Downloaded ✔';
+                    button.classList.add('btn-success');
+                    // button.href = 'javascript:void()'
+                }
+    <?php 
+            }
+        }
+    ?>
+    
+</script>
 </html>
 <?php
  } else {
 
-    header('location:index.html');
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="Assets/style.css">
+    <link rel="icon" href="Assets/icoo.ico" type="image/x-icon">
+    <title>Encrypt/Decrypt Download</title>
+</head>
+<body>
+<div class="container">
+        <div class="jumbotron jumbotron-modified">
+            <h1 class="display-4">Hello, There!</h1>
+            <p class="lead">This website allows you to encrypt and decrypt(<span class="span-modified">which is
+                    encrypted using this website</span>) kind of data.</p>
+            <hr class="my-4">
+            <p>
+                <span class="span-modified">*NOTE : </span>It ecrypts the data and it never keeps any backup in the
+                cloud
+                and it's an open source software which means that you can view the source code of this website. To view
+                the source code <a href="https://github.com/ManojAvarur/Cloud-Encryption" target="_blank"
+                    class="text-warning modified-text"> click here </a>
+            </p>
+            <hr class="my-4">
+            <div class="container">
+
+                    <div class="card text-center" style="background-color:  #1d1d1d;">
+
+                        <div class='card-body'>
+                            <h5 class='card-title'>Who are you?</h5>
+                            <p class='card-text'>We could'nt find any of your files on our server. Please try again!</p>
+                            <a href='index.html' class='btn btn-primary'>Go Back</a>
+                        </div>
+                    
+
+                    </div>
+            
+            </div>
+            <hr class="my-4">
+                    <p style="text-align: center;">
+                        <span class="span-modified">*NOTE : </span> If the file is not downloaded after the computation the data
+                        will be deleted and it cannot be accessed by the user.
+                    </p>
+
+                    <hr class="my-4">
+                    <p style="text-align: center;">
+                        <span class="span-modified">*NOTE : </span> If the enterd password is incorrect then the file will also be corrupted.
+                    </p>
+        </div>
+</div>
+</body>
+</html>
+
+<?php
 
  }
+
+ ?>
+  
