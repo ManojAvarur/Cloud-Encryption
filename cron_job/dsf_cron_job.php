6d3a8e51eb610fd745812c1f6f48fc247f5b2fbf
Manoj A M <manojavarur@gmail.com>
2021-03-25 22:34:33 +0530

    Change in Cron Job of removing files from ToShare folder

--- cron_job/dsf_cron_job.php
+++ cron_job/dsf_cron_job.php
@@ -1,8 +1,31 @@
 <?php
 
+    // class My extends Thread {
+    //     public function run() {
+    //         $location = '../ToShare';
+    //         $files = scandir($location,1);
+
+    //         // print_r($files);
+
+    //         for( $i = 0; $i < count( $files ); $i++ ){
+
+    //             if ( time() - filemtime( $location.'/'.$files[$i] )  > 86400 )
+    //                 unlink( $location.'/'.$files[$i] );
+                        
+    //         }
+
+    //     }
+    // }
+
+
     function deleteFile( $location ){
+
+        // $my = new My();
+        // $my->start();
  
         $files = scandir($location,1);
+
+        // print_r($files);
 
         for( $i = 0; $i < count( $files ); $i++ ){
 
@@ -10,13 +33,31 @@
                 unlink( $location.'/'.$files[$i] );
                     
         }
+
+
     }
 
     // $files = scandir('../ToShare',1);
-    date_default_timezone_set('Asia/Kolkata');
+   
     // $files = scandir('D:/Movies',1);
 
     // for( $i = 0; $i < count( $files ); $i++ )
-    //     deleteFile( $files[$i] );   
+    //     deleteFile( $files[$i] );  
+
+    // $file = fopen( 'visitors.txt', 'r');
+    // $visits = fscanf($file,"%d");
+    // fclose($file);
+
+    // $visits[0]++;
+    
+    // $file = fopen( 'visitors.txt', 'w');
+    // fwrite($file,$visits[0]);
+            
+    // fclose($file); 
+
+    echo "Hello World";
+
+    date_default_timezone_set('Asia/Kolkata');
 
     deleteFile('../ToShare');
+
